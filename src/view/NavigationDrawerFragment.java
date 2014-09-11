5501ffb40b8f2d1a43c1012ff8c424652316330c
oldoldb <xue-yajie@163.com>
2014-09-11 11:09:51 +0800

    update ui

--- src/view/NavigationDrawerFragment.java
+++ src/view/NavigationDrawerFragment.java
@@ -21,7 +21,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -247,12 +246,6 @@ public class NavigationDrawerFragment extends Fragment {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
-        if (item.getItemId() == R.id.action_add_item) {
-            Toast.makeText(getActivity(), "Add Item.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
